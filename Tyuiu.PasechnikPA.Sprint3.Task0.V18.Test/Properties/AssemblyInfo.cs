19d5e6d883c621ad64e9e5eb18488877c3be7513
Pavel Pasechnik <Tyuiu.PasechnikPA@outlook.com>
2023-10-21 22:29:30 +0500

    Add project files.

---
+++ Tyuiu.PasechnikPA.Sprint3.Task0.V18.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PasechnikPA.Sprint3.Task0.V18.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PasechnikPA.Sprint3.Task0.V18.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("112c7f34-9e75-4431-b7b4-55f4e0430e81")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.PasechnikPA.Sprint3.Task0.V18/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Tyuiu.PasechnikPA.Sprint3.Task0.V18.Lib;
+
+namespace Tyuiu.PasechnikPA.Sprint3.Task0.V18
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Пасечник П. А. | АСОиУБ-23-2";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла for                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Варинант #18                                                            *");
+            Console.WriteLine("* Выполнил: Пасечник Павел Андреевич | АСОиУБ-23-2                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*Написать программу используя цикл for, которая вычисляет произведение*****");
+            Console.WriteLine("*ряда по формуле, при X=1**************************************************");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int value = 1;
+            int startValue = 1;
+            int stopValue = 6;
+
+            Console.WriteLine("Переменная X = "+value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Произведение ряда = " + ds.GetMultiplySeries(value,startValue,stopValue));
+            Console.ReadLine();
+        }
+    }
+}
